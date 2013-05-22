3069080e678b89fd68ffc5f96c2548163ddfec63
Hidetomo Suzuki <hidetomo@ip-10-250-20-92.us-west-2.compute.internal>
2013-05-22 11:23:03 +0000

    edge detection

---
+++ edge-detection-laplacian/laplacian.rb
@@ -0,0 +1,41 @@
+require 'imlib2'
+
+img = Imlib2::Image.load("srcimg/lena.jpg")
+
+img.h.times do |y|
+    img.w.times do |x|
+        color = img.pixel(x, y)
+        
+        gray = (0.299 * color.r + 0.587 * color.g + 0.114 * color.b).to_i
+        color.r = color.g = color.b = gray
+
+        img.draw_pixel(x, y, color)
+    end
+end
+
+laplacian_filter=[[1,1,1],[1,-8,1],[1,1,1]]
+img.h.times do |y|
+    img.w.times do |x|
+        if x == 0 or x == img.width
+            next
+        end
+        if y == 0 or y == img.height
+            next
+        end
+        
+        sum = 0
+        for i in -1..1 do
+            for j in -1..1 do
+                color = img.pixel(x+j, y+i)
+                sum = color.r * laplacian_filter[i+1][j+1]
+            end
+        end
+        color = img.pixel(x,y)
+        color.r = color.b = color.g = sum
+
+        img.draw_pixel(x, y, color)
+    end
+end
+
+
+img.save("dstimg/edge.jpg")
